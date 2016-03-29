c10718a7599d575838028ef38862d7b61d8debb1
Derek Newsom <newsom.derek@gmail.com>
2016-03-29 10:14:59 -0700

    Lambda runtime to execute component checks and send email if anything is not fully operational

--- uptime-tracker/index.js
+++ uptime-tracker/index.js
@@ -5,8 +5,63 @@
 var dotenv = require('dotenv');
 dotenv.load();
 
+var StatusChecker = require('./StatusChecker.js');
+var EmailNotify = require('./EmailNotify.js');
+var Components;
+
+try {
+    Components = require('./Components.js');
+} catch (ex) {
+
+}
 
 exports.handler = function(event, context) {
-    console.log('lambda method handler');
-    context.succeed();
+
+    if(Components) {
+
+        Components.checks.map(function(check) {
+            StatusChecker.addComponent(check.componentLabel, check.groupId, check.method, check.componentId, check.performanceLimit);
+        });
+
+        Components.groups.map(function(group) {
+            StatusChecker.addComponentGroup(group.groupId, group.groupLabel);
+        });
+
+        StatusChecker.getStatusReport(function(statusReport) {
+
+            var allChecksOperational = true;
+
+            // confirm all component status checks are operational
+            statusReport.status.map(function(check) {
+                if(check.status !== StatusChecker.getStatusCodes().other) {
+                    allChecksOperational = false;
+                }
+            });
+
+            if(!allChecksOperational) {
+                var notify = new EmailNotify();
+                var wordified = notify.wordifyReport(statusReport);
+
+                var fromEmail = process.env.DISCORD_NOTIFY_FROM_EMAIL;
+                var toEmail = process.env.DISCORD_NOTIFY_TO_EMAILS.split(',');
+                var subject = process.env.DISCORD_NOTIFY_SUBJECT;
+                var bodyText = wordified.text;
+                var bodyHtml = wordified.html;
+
+                notify.send(
+                    fromEmail, toEmail, subject, bodyText, bodyHtml,
+                    function(err, data) {
+                        context.succeed();
+                    }
+                );
+            } else {
+                context.succeed();
+            }
+
+        });
+
+    } else {
+        context.succeed();
+    }
+
 };
